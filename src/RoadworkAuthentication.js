da4a9d0535cd596ae7613863fa9a643dee88583a
Xavier Geerinck <thebillkidy@gmail.com>
2016-09-18 12:01:32 +0200

    ES6

--- src/RoadworkAuthentication.js
+++ src/RoadworkAuthentication.js
@@ -3,279 +3,281 @@ const Boom = require('boom');
 const dbUtil = require('./db/utils/dbUtil');
 const pluralize = require('pluralize');
 
-var RoadworkAuthentication = function (server, bookshelf) {
-    if (!server) {
-        throw new Error('Missing the http engine');
+class RoadworkAuthentication {
+    constructor (server, bookshelf) {
+        if (!server) {
+            throw new Error('Missing the http engine');
+        }
+
+        if (!bookshelf) {
+            throw new Error('Missing the bookshelf object');
+        }
+
+        this.server = server;
+        this.strategyName = 'roadwork-authentication-bearer';
+        this.bookshelf = bookshelf;
+
+        this.models = {};
+        this.models.UserModel = require('./db/models/User')(this.bookshelf);
+        this.models.UserSessionModel = require('./db/models/UserSession')(this.bookshelf);
+
+        this.controllers = {};
+        this.controllers.AuthController = require('./controllers/AuthController')(this.models);
     }
 
-    if (!bookshelf) {
-        throw new Error('Missing the bookshelf object');
+    getStrategyName () {
+        return this.strategyName;
     }
 
-    this.server = server;
-    this.strategyName = 'roadwork-authentication-bearer';
-    this.bookshelf = bookshelf;
-
-    this.models = {};
-    this.models.UserModel = require('./db/models/User')(this.bookshelf);
-    this.models.UserSessionModel = require('./db/models/UserSession')(this.bookshelf);
-
-    this.controllers = {};
-    this.controllers.AuthController = require('./controllers/AuthController')(this.models);
-};
-
-RoadworkAuthentication.prototype.getStrategyName = function () {
-    return this.strategyName;
-};
-
-/**
- * Registers the routes of a passed require() routes file to the engine
- * @param routesFile
- */
-RoadworkAuthentication.prototype.addRoute = function (routesFile) {
-    var self = this;
-    console.log(routesFile);
-    for (var route in routesFile) {
-        console.info('[x] route: ' + routesFile[route].method + ' ' + routesFile[route].path);
-        self.server.route(routesFile[route]);
+    /**
+     * Registers the routes of a passed require() routes file to the engine
+     * @param routesFile
+     */
+    addRoute (routesFile) {
+        var self = this;
+        console.log(routesFile);
+        for (var route in routesFile) {
+            console.info('[x] route: ' + routesFile[route].method + ' ' + routesFile[route].path);
+            self.server.route(routesFile[route]);
+        }
     }
-};
 
-/**
- * Init the library, this will perform some checks, and also init some pre-existing functionality such as the login routes
- */
-RoadworkAuthentication.prototype.init = function () {
-    var self = this;
+    /**
+     * Init the library, this will perform some checks, and also init some pre-existing functionality such as the login routes
+     */
+    init () {
+        var self = this;
 
-    console.log('===========================================');
-    console.log('Initializing Roadwork Authentication Plugin');
-    console.log('===========================================');
-    console.log('Checking the required scheme');
-    return self.checkRequiredScheme()
-    .then(() => {
-        console.log('Adding routes');
-        // Add routes
-        self.addRoute(require('./routes/auth')(self.controllers.AuthController));
+        console.log('===========================================');
+        console.log('Initializing Roadwork Authentication Plugin');
+        console.log('===========================================');
+        console.log('Checking the required scheme');
+        return self.checkRequiredScheme()
+            .then(() => {
+                console.log('Adding routes');
+                // Add routes
+                self.addRoute(require('./routes/auth')(self.controllers.AuthController));
 
 
-        return Promise.resolve();
-    })
-    .catch((err) => {
-        return Promise.reject(err);
-    });
-};
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                return Promise.reject(err);
+            });
+    }
 
-/**
- * Check if the required tables and columns exist in the database. These are:
- * user
- *     - id
- * user_session
- *     - user_id
- *     - token
- *
- * The non existent tables will be created by the system
- * @returns boolean
- */
-RoadworkAuthentication.prototype.checkRequiredScheme = function () {
-    // Create the user table if needed or if it has missing columns
-    return this.bookshelf.knex.schema.hasTable('user')
-    .then((exists) => {
-        if (exists) {
-            console.info('[x] User table exists');
-            return this.checkUserTableScheme();
-        }
-
-        console.info('[x] Creating User table');
-        return this.createUserTable();
-    })
-    // Create the user_session table if needed or if it has missing columns
-    .then(() => {
-        console.info('[x] User table required columns exist');
-        return this.bookshelf.knex.schema.hasTable('user_session');
-    })
-    .then((exists) => {
-        if (exists) {
-            console.info('[x] UserSession table exists');
-            return this.checkUserSessionScheme();
-        }
-
-        console.info('[x] Creating UserSession table');
-        return this.createUserSessionTable();
-    })
-    .then(() => {
-        console.info('[x] UserSession table required columns exist');
-        return Promise.resolve();
-    })
-    .catch((err) => {
-        return Promise.reject(err);
-    });
-};
-
-RoadworkAuthentication.prototype.checkUserTableScheme = function () {
-    var tableName = 'user';
-    var requiredColumns = [ 'id' ];
-
-    return dbUtil.columnsExist(this.bookshelf.knex, tableName, requiredColumns);
-};
-
-RoadworkAuthentication.prototype.checkUserSessionScheme = function () {
-    var tableName = 'user_session';
-    var requiredColumns = [ 'user_id', 'token' ];
-
-    return dbUtil.columnsExist(this.bookshelf.knex, tableName, requiredColumns);
-};
-
-RoadworkAuthentication.prototype.createUserTable = function () {
-    return dbUtil.createTableByScheme(this.bookshelf.knex, require('./db/schemas/user'), 'user');
-};
-
-RoadworkAuthentication.prototype.createUserSessionTable = function () {
-    return dbUtil.createTableByScheme(this.bookshelf.knex, require('./db/schemas/user_session'), 'user_session');
-};
-
-/**
- * The validate function to be used to see if a user has access to a specific route
- * @param token
- * @param callback(err, isValid, credentials)
- */
-RoadworkAuthentication.prototype.validateFunction = function (token, callback) {
-    var self = this;
-
-    this.models
-    .UserSessionModel
-    .where({ token: token })
-    .fetch()
-    .then(function (userSession) {
-        if (!userSession) {
-            return Promise.reject(Boom.badRequest('INVALID_TOKEN'));
-        }
-
-        return self.models.UserModel.where({ id: userSession.get('user_id') }).fetch();
-    })
-    .then(function (user) {
-        if (!user) {
-            return Promise.reject(Boom.badRequest('INVALID_TOKEN'));
-        }
-
-        var userObj = user;
-        userObj.scope = [ user.get('scope') ];
-        userObj.scope.push('user-' + userObj.id);
-
-        return callback(null, true, userObj);
-    })
-    .catch(function (err) {
-        return callback(err);
-    });
-};
-
-RoadworkAuthentication.prototype.hasAccess = function (request, rolesAllowed, model) {
-    var self = this;
-
-    return new Promise((resolve, reject) => {
-        let credentials = request.auth.credentials;
-        let rowId = request.path.split('/')[2];
-
-        self.getTableRowOwner(model.tableName, rowId)
-        .then((ownerId) => {
-            if (credentials && parseInt(ownerId) === parseInt(credentials.get('id'))) {
-                return resolve(true);
-            }
-
-            return resolve(false);
-        });
-    });
-};
-
-RoadworkAuthentication.prototype.getTableRowOwner = function (table, rowId) {
-    // If it's the main user table, return the id
-    if (table === 'user') {
-        return Promise.resolve(rowId);
-    } else {
-        return new Promise((resolve, reject) => {
-            this.bookshelf.knex.select('user_id').from(table).where('id', rowId)
-            .then((result) => {
-                if (!result || result.length == 0) {
-                    return resolve(null);
+    /**
+     * Check if the required tables and columns exist in the database. These are:
+     * user
+     *     - id
+     * user_session
+     *     - user_id
+     *     - token
+     *
+     * The non existent tables will be created by the system
+     * @returns boolean
+     */
+    checkRequiredScheme () {
+        // Create the user table if needed or if it has missing columns
+        return this.bookshelf.knex.schema.hasTable('user')
+            .then((exists) => {
+                if (exists) {
+                    console.info('[x] User table exists');
+                    return this.checkUserTableScheme();
                 }
 
-                return resolve(result[0].user_id);
+                console.info('[x] Creating User table');
+                return this.createUserTable();
+            })
+            // Create the user_session table if needed or if it has missing columns
+            .then(() => {
+                console.info('[x] User table required columns exist');
+                return this.bookshelf.knex.schema.hasTable('user_session');
+            })
+            .then((exists) => {
+                if (exists) {
+                    console.info('[x] UserSession table exists');
+                    return this.checkUserSessionScheme();
+                }
+
+                console.info('[x] Creating UserSession table');
+                return this.createUserSessionTable();
+            })
+            .then(() => {
+                console.info('[x] UserSession table required columns exist');
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                return Promise.reject(err);
             });
+    }
+
+    checkUserTableScheme () {
+        var tableName = 'user';
+        var requiredColumns = [ 'id' ];
+
+        return dbUtil.columnsExist(this.bookshelf.knex, tableName, requiredColumns);
+    }
+
+    checkUserSessionScheme () {
+        var tableName = 'user_session';
+        var requiredColumns = [ 'user_id', 'token' ];
+
+        return dbUtil.columnsExist(this.bookshelf.knex, tableName, requiredColumns);
+    }
+
+    createUserTable () {
+        return dbUtil.createTableByScheme(this.bookshelf.knex, require('./db/schemas/user'), 'user');
+    }
+
+    createUserSessionTable () {
+        return dbUtil.createTableByScheme(this.bookshelf.knex, require('./db/schemas/user_session'), 'user_session');
+    }
+
+    /**
+     * The validate function to be used to see if a user has access to a specific route
+     * @param token
+     * @param callback(err, isValid, credentials)
+     */
+    validateFunction (token, callback) {
+        var self = this;
+
+        this.models
+            .UserSessionModel
+            .where({ token: token })
+            .fetch()
+            .then(function (userSession) {
+                if (!userSession) {
+                    return Promise.reject(Boom.badRequest('INVALID_TOKEN'));
+                }
+
+                return self.models.UserModel.where({ id: userSession.get('user_id') }).fetch();
+            })
+            .then(function (user) {
+                if (!user) {
+                    return Promise.reject(Boom.badRequest('INVALID_TOKEN'));
+                }
+
+                var userObj = user;
+                userObj.scope = [ user.get('scope') ];
+                userObj.scope.push('user-' + userObj.id);
+
+                return callback(null, true, userObj);
+            })
+            .catch(function (err) {
+                return callback(err);
+            });
+    }
+
+    hasAccess (request, rolesAllowed, model) {
+        var self = this;
+
+        return new Promise((resolve, reject) => {
+            let credentials = request.auth.credentials;
+            let rowId = request.path.split('/')[2];
+
+            self.getTableRowOwner(model.tableName, rowId)
+                .then((ownerId) => {
+                    if (credentials && parseInt(ownerId) === parseInt(credentials.get('id'))) {
+                        return resolve(true);
+                    }
+
+                    return resolve(false);
+                });
         });
     }
-};
 
-/**
- * Create the implementation to be used in the hapi framework
- */
-RoadworkAuthentication.prototype.getHapiFrameworkInterface = function () {
-    return require('./adapters/hapi-interface')(this);
-};
+    getTableRowOwner (table, rowId) {
+        // If it's the main user table, return the id
+        if (table === 'user') {
+            return Promise.resolve(rowId);
+        } else {
+            return new Promise((resolve, reject) => {
+                this.bookshelf.knex.select('user_id').from(table).where('id', rowId)
+                    .then((result) => {
+                        if (!result || result.length == 0) {
+                            return resolve(null);
+                        }
 
-/**
- * Validates the headers and queryParams its access_token. This can be passed in the header through:
- * Authorization: Bearer <token>
- *
- * or in the query through:
- * ?access_token=<token>
- *
- * the rules of parsing: QueryParameters > Authorization Header
- *
- * @param headers
- * @param queryParams
- * @param callback(err, credentialsObject)
- * @returns {*}
- */
-RoadworkAuthentication.prototype.validateBearerToken = function (headers, queryParams, callback) {
-    var self = this;
-
-    let bearerToken = '';
-
-    if (queryParams.access_token) {
-        bearerToken = queryParams.access_token;
-        //delete queryParams.access_token;
+                        return resolve(result[0].user_id);
+                    });
+            });
+        }
     }
-    else if (headers.authorization && headers.authorization !== undefined) {
-        const headerParts = headers.authorization.split(' ');
 
-        if (headerParts[0].toLowerCase() !== 'bearer') {
+    /**
+     * Create the implementation to be used in the hapi framework
+     */
+    getHapiFrameworkInterface () {
+        return require('./adapters/hapi-interface')(this);
+    }
+
+    /**
+     * Validates the headers and queryParams its access_token. This can be passed in the header through:
+     * Authorization: Bearer <token>
+     *
+     * or in the query through:
+     * ?access_token=<token>
+     *
+     * the rules of parsing: QueryParameters > Authorization Header
+     *
+     * @param headers
+     * @param queryParams
+     * @param callback(err, credentialsObject)
+     * @returns {*}
+     */
+    validateBearerToken (headers, queryParams, callback) {
+        var self = this;
+
+        let bearerToken = '';
+
+        if (queryParams.access_token) {
+            bearerToken = queryParams.access_token;
+            //delete queryParams.access_token;
+        }
+        else if (headers.authorization && headers.authorization !== undefined) {
+            const headerParts = headers.authorization.split(' ');
+
+            if (headerParts[0].toLowerCase() !== 'bearer') {
+                return callback(Boom.unauthorized(null, this.strategyName));
+            }
+
+            bearerToken = headerParts[1];
+        }
+        else {
             return callback(Boom.unauthorized(null, this.strategyName));
         }
 
-        bearerToken = headerParts[1];
-    }
-    else {
-        return callback(Boom.unauthorized(null, this.strategyName));
-    }
-
-    return self.validateFunction(bearerToken, (err, isValid, credentials) => {
-        //console.log(err);
-        //console.log(isValid);
-        //console.log(credentials);
-        return self.validateCallback(err, isValid, credentials, bearerToken, callback);
-    });
-};
-
-RoadworkAuthentication.prototype.validateCallback = function (err, isValid, credentials, token, callback) {
-    credentials = credentials || null;
-
-    if (err) {
-        return callback(err, credentials);
+        return self.validateFunction(bearerToken, (err, isValid, credentials) => {
+            //console.log(err);
+            //console.log(isValid);
+            //console.log(credentials);
+            return self.validateCallback(err, isValid, credentials, bearerToken, callback);
+        });
     }
 
-    if (!isValid) {
-        return callback(Boom.unauthorized(null, this.strategyName, {
-            isValid,
-            credentials
-        }), credentials);
+    validateCallback (err, isValid, credentials, token, callback) {
+        credentials = credentials || null;
+
+        if (err) {
+            return callback(err, credentials);
+        }
+
+        if (!isValid) {
+            return callback(Boom.unauthorized(null, this.strategyName, {
+                isValid,
+                credentials
+            }), credentials);
+        }
+
+        if (!credentials || typeof credentials !== 'object') {
+            return callback(Boom.badImplementation('Bad credentials object received for bearerAuth auth validation'));
+        }
+
+        credentials.token = token;
+
+        return callback(null, credentials);
     }
-
-    if (!credentials || typeof credentials !== 'object') {
-        return callback(Boom.badImplementation('Bad credentials object received for bearerAuth auth validation'));
-    }
-
-    credentials.token = token;
-
-    return callback(null, credentials);
-};
+}
 
 module.exports = RoadworkAuthentication;
--- src/adapters/hapi-interface.js
+++ src/adapters/hapi-interface.js
@@ -3,65 +3,6 @@ const pluralize = require('pluralize');
 
 module.exports = function (roadworkAuthenticationScope) {
     var register = (server, options, next) => {
-        /**
-         * Create a onPreAuth hook, that will resolve the $owner role required into a user-<id> role required, depending on the requested object
-         */
-        // server.ext('onPreAuth', (request, reply) => {
-        //     // If no auth defined, continue
-        //     if (!request.route.settings.auth) {
-        //         return reply.continue();
-        //     }
-        //
-        //     var table = pluralize.singular(request.path.split('/')[1]);
-        //     var rowId = request.path.split('/')[2];
-        //
-        //     // If we found no rowId, then it means that it is a findAll or create call
-        //     // These are special, since the findAll should then return all the routes that belong to a specific user
-        //     // Whereas the $owner role on a create call is impossible (since no objects exist)
-        //     if (!rowId) {
-        //         if (!request.route.settings.auth || !request.route.settings.auth.access || !request.route.settings.auth.access[0].scope || !request.route.settings.auth.access[0].scope.selection) {
-        //             return reply.continue();
-        //         }
-        //
-        //         // Filter out the $owner role, it's not needed here since we will define this in the main plugin
-        //         request.route.settings.auth.access[0].scope.selection = request.route.settings.auth.access[0].scope.selection.filter((item) => {
-        //             return item != '$owner';
-        //         });
-        //
-        //         if (request.route.settings.auth.access[0].scope.selection.length == 0) {
-        //             request.route.settings.auth.access[0].scope.selection = undefined;
-        //         }
-        //
-        //         return reply.continue();
-        //     }
-        //
-        //     // Allow the table owner access
-        //     roadworkAuthenticationScope.getTableRowOwner(table, rowId)
-        //     .then((ownerId) => {
-        //         console.log('ownerId: ' + ownerId);
-        //         if (!ownerId) {
-        //             // Return no resource http response
-        //             return reply(Boom.notFound());
-        //         }
-        //
-        //         // TODO: We can not do this, then we will change the role for everyone!
-        //         // Add the dynamic role
-        //         for (let i in request.route.settings.auth.access[0].scope.selection) {
-        //             if (request.route.settings.auth.access[0].scope.selection[i] !== '$owner') {
-        //                 continue;
-        //             }
-        //
-        //             request.route.settings.auth.access[0].scope.selection[i] = 'user-' + ownerId;
-        //         }
-        //
-        //         // Current resolved $owner scope
-        //         console.log(request.route.settings.auth.access[0].scope.selection);
-        //
-        //         // return reply
-        //         reply.continue();
-        //     });
-        // });
-
         server.auth.scheme(roadworkAuthenticationScope.strategyName, (server, options) => {
             return {
                 authenticate: (request, reply) => {
